9841e50b633326d74d2355871e1fea4fcdb7d172
Jahaan-boop <85781919+Jahaan-boop@users.noreply.github.com>
2022-08-08 16:17:21 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,22 +2,24 @@ function preload(){
 
 }
 
-function draw(){
-circle(30, 30, 50);
-circle(400, 400, 50);
-rect(30, 300, 55, 55, 20);
-rect(90, 400, 55, 55);
-circle(90, 200, 50)
-}
-
 function take_snapshot(){
 save("my_img.png")
 }
 
 function setup(){
-    
 canvas = createCanvas(650,500);
 canvas.position(100,250);
 video=createCapture(VIDEO);
 video.hide();
+filter="";
+}
+
+function draw(){
+    image(video,150,100,350,300);
+    fill("purple");
+    stroke("purple");
+    circle(50,50,80);
+    circle(50,450,80);
+    circle(580,50,80);
+    circle(580,450,80);
 }
